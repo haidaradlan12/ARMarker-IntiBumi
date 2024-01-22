f7f8fe132f9a4c4cd5cd0c52afb5d9f5500cbdaa
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 16:32:05 +0700

    cek

---
+++ Assets/Asset Balok Script/UIManager2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager2 : MonoBehaviour
+{
+    public RectTransform MenuMenuButton, MenuStruktur, MenuKerak, MenuMantel, MenuLuar, MenuDalam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuKembalibutton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuStrukturButton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuKerakButton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuMantelButton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuLuarButton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void MenuDalamButton()
+    {
+        MenuMenuButton.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuStruktur.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuKerak.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuMantel.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuLuar.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuDalam.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+}
---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform MenuGambarKiri, MenuGambarKanan, MenuGambarBawah, MenuButton, MenuInformasi, MenuPetunjuk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuGambarKiri.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuGambarKanan.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuGambarBawah.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void MenuKembalibutton()
+    {
+        MenuGambarKiri.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuGambarKanan.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuGambarBawah.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void MenuInformasiButton()
+    {
+        MenuGambarKiri.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuGambarKanan.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuGambarBawah.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void MenuPetunjukButton()
+    {
+        MenuGambarKiri.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuGambarKanan.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        MenuGambarBawah.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+}
